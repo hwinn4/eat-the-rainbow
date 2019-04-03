dec692f51ea22ad73a2e628ddd2df25c24f6c021
Hilary Winn <hilary@fuigo.com>
2019-04-03 07:44:30 -0400

    updates foods migration

--- db/migrate/20190403010545_create_foods.rb
+++ db/migrate/20190403010545_create_foods.rb
@@ -1,7 +1,7 @@
 class CreateFoods < ActiveRecord::Migration[5.2]
   def change
     create_table :foods do |t|
-      t.belongs_to :user
+      t.belongs_to :user, index: true, foreign_key: true
       t.integer :color
       t.date :date, null: false
 
