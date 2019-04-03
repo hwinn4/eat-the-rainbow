63423192937411eaa80d7ffe8ea33f910f104bdb
Hilary Winn <hilary@fuigo.com>
2019-04-03 09:37:09 -0400

    wip applies styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def color_map
+    {
+        'red' => '#B22230',
+        'orange' => '#FEA56C',
+        'yellow' => '#EBD175',
+        'green' => '#8DA05E',
+        'blue' => '#9AABBD',
+        'purple' => '#814F76',
+        'black' => '#26272A',
+        'white' => '#ECEBEB',
+        'tan' => '#C9A382',
+        'brown' => '#664C3E'
+    }
+  end
 end
