d39c52c1f124c5f0841bf805619cba7e15e46286
Hilary Winn <hilary@fuigo.com>
2019-04-02 22:03:28 -0400

    defines color enum on foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,17 @@ class Food < ApplicationRecord
   belongs_to :user
   validates_presence_of :date
   validates_presence_of :color
+
+  enum color: %i[
+    red
+    orange
+    yellow
+    green
+    blue
+    purple
+    black
+    white
+    tan
+    brown
+  ]
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Food do
   it { should validate_presence_of(:date) }
   it { should validate_presence_of(:color) }
+  it { should define_enum_for(:color) }
 end
