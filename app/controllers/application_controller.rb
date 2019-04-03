078acc102dfebe5fd66c8d6b2c3d25dfaafd66ff
Hilary Winn <hilary@fuigo.com>
2019-04-02 23:27:37 -0400

    sets @user var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  include Devise::Controllers::Helpers
+
   before_action :authenticate_user!
+  before_action :set_current_user
   protect_from_forgery prepend: true
 
   def login
     render 'layouts/index'
+  end
+
+  def set_current_user
+    @user = current_user
   end
 end
