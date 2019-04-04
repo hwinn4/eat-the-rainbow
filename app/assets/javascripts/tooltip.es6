1d055047c954bd3a6aec127040cd911f9947eea2
Hilary Winn <hilary@fuigo.com>
2019-04-04 17:42:16 -0400

    adds tooltip with color count for daily log

---
+++ app/assets/javascripts/tooltip.es6
@@ -0,0 +1,7 @@
+$( document ).on("turbolinks:load", () => {
+  $(() => {
+    $('[data-toggle="tooltip"]').tooltip();
+  })
+});
+
+
--- app/models/null_food.rb
+++ app/models/null_food.rb
@@ -1,6 +1,7 @@
 class NullFood
-  attr_reader :color
+  attr_reader :color, :color_count
   def initialize(color)
     @color = color
+    @color_count = 0
   end
 end
