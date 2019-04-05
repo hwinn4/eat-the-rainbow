6db79f330e8c410c8f56706a9112aa148e7d540d
Hilary Winn <hilary@fuigo.com>
2019-04-05 10:40:49 -0400

    moves logic into helper

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,8 +5,7 @@ class FoodsController < ApplicationController
     date = params[:food] ? food_query_params[:date] : Date.today
     @date = index_date(date)
 
-    foods = FoodByDay.search(current_user.id, @date)
-    log = DailyFoodLog.new(foods, current_user.id, @date)
+    log = build_food_log
     @foods = log.sorted_full_day_log
     @should_celebrate = log.has_all_colors?
     @colors = Food.colors
@@ -17,11 +16,8 @@ class FoodsController < ApplicationController
   def create
     params['food']['date'] = string_to_date(new_food_params[:date])
     food = current_user.foods.build(new_food_params)
-    # TODO: Else!
-    if food.save
-      # TODO: Use render or redirect_to?
-      redirect_to "/foods?food[date]=#{date_to_string(food.date)}"
-    end
+    food.save
+    redirect_to "/foods?food[date]=#{date_to_string(food.date)}"
   end
 
   def destroy
@@ -30,6 +26,12 @@ class FoodsController < ApplicationController
   end
 
   private
+
+  def build_food_log
+    foods = FoodByDay.search(current_user.id, @date)
+    DailyFoodLog.new(foods, current_user.id, @date)
+  end
+
   def food_query_params
     params.require(:food).permit(:date)
   end
